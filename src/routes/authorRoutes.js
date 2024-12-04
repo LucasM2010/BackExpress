34f367c1019f6c77b6495d1c8d6c74fb710cefac
LucasM2010 <131216534+LucasM2010@users.noreply.github.com>
2024-12-04 13:47:41 -0300

    Adicionando Rotas

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authorController = require('../controllers/authorController');
 
 router.post('/', authorController.createAuthor);
-
+router.get('/', authorController.listAuthors);
 
 module.exports = router;
