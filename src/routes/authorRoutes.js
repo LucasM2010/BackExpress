de5a83d4cb627d2ec20009111afa58949e2efeda
matheusantonio156 <164435308+matheusantonio156@users.noreply.github.com>
2024-12-04 14:29:15 -0300

    atualização

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authorController = require('../controllers/authorController');
 
+// Rotas de autores
 router.post('/', authorController.createAuthor);
 router.get('/', authorController.listAuthors);
+router.put('/:id', authorController.updateAuthor); // Atualizar autor
+router.delete('/:id', authorController.deleteAuthor); // Deletar autor
 
 module.exports = router;
