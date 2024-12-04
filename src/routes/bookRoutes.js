e6795cb040a3618cf6e0b0eb88e80c0b1585382d
LucasM2010 <131216534+LucasM2010@users.noreply.github.com>
2024-12-04 14:27:33 -0300

    Novas rotas

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
 
+// Rotas de livros
 router.post('/', bookController.createBook);
 router.get('/', bookController.listBooks);
+router.put('/:id', bookController.updateBook); // Atualizar livro
+router.delete('/:id', bookController.deleteBook); // Deletar livro
 
 module.exports = router;
