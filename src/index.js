629505713f34d3a27873ca1bdb142c26ab563acc
LucasM2010 <lucasggl2010@gmail.com>
2024-12-04 12:42:07 -0300

    Atualizações

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 require('dotenv').config();
 const express = require('express');
+const { Pool } = require('pg');
 const authorRoutes = require('./routes/authorRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 
 const app = express();
 app.use(express.json());
+
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+
+pool.connect()
+  .then(() => console.log('Conectado ao banco de dados PostgreSQL'))
+  .catch(err => console.error('Erro ao conectar ao banco de dados', err));
+
 
 app.use('/authors', authorRoutes);
 app.use('/books', bookRoutes);
