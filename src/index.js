4fc3469d6a61016582118c0b88f36d21002963a0
LucasM2010 <131216534+LucasM2010@users.noreply.github.com>
2024-12-04 13:51:32 -0300

    Arrumando conflitos

--- src/index.js
+++ src/index.js
@@ -55,5 +55,6 @@ app.use('/books', bookRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(Servidor rodando na porta ${PORT});
+  console.log(`Servidor rodando na porta ${PORT}`);
+
 });
